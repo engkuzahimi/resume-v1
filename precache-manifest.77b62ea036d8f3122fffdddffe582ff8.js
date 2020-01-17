f8aaa463560e4e2060b548511bba1406185a16c0
Zahimi <engkuzahimi@gmail.com>
2020-01-18 02:21:19 +0800

    Updates

--- precache-manifest.77b62ea036d8f3122fffdddffe582ff8.js
+++ precache-manifest.77b62ea036d8f3122fffdddffe582ff8.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f9f9e1695231097463b3ee57e64da53b",
+    "revision": "7923505165a17c8fe91349ef9a3b9f4e",
     "url": "/resume-v1/index.html"
   },
   {
-    "revision": "6a3fb054cb759f534221",
-    "url": "/resume-v1/static/css/main.d3e206c6.chunk.css"
+    "revision": "96e993d3db2071e0aff9",
+    "url": "/resume-v1/static/css/main.b00a963c.chunk.css"
   },
   {
     "revision": "e86c21a244d03c6e3638",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/resume-v1/static/js/2.c15867d4.chunk.js.LICENSE"
   },
   {
-    "revision": "6a3fb054cb759f534221",
+    "revision": "96e993d3db2071e0aff9",
     "url": "/resume-v1/static/js/main.3904f1b9.chunk.js"
   },
   {
