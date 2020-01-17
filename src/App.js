8bd261fda3a6069c529c22c135c6f7706fb1cc0a
Zahimi <engkuzahimi@gmail.com>
2020-01-18 03:02:27 +0800

    fixed all styling, added pdf button

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import React from 'react';
 import './App.css';
 import Right from './right';
 import Left from './left';
-import AOS from 'aos';
 
 function App() {
-  AOS.init();
   return (
 
     <div className="mainPage">
--- src/left.js
+++ src/left.js
@@ -6,14 +6,12 @@ export class Left extends Component {
     render() {
         return (
                 <div className="left-content">
-                    <div className="forMobile">
                     <h2>Hi, I am Zahimi!</h2>
-                    <p>Information Technology Student @  Universiti Teknologi Petronas</p>
+                    <p>Information Technology Student @ <br /> Universiti Teknologi Petronas</p>
                     <p>Looking for Internship oppurtunities (May 2020 - December 2020)</p>
                     <a href="google.com"><i class="fas fa-mobile-alt"></i></a>
+                    <a href="https://google.com" className="pdfButton">Get my resume in PDF!</a>
                     </div>
-                    </div>              
-              
         )
     }
 }
