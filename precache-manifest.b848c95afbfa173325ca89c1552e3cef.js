08c1eb0e573198fd958048ae09d5e8b6060e7e9c
Zahimi <engkuzahimi@gmail.com>
2020-01-18 01:26:00 +0800

    Updates

---
+++ precache-manifest.b848c95afbfa173325ca89c1552e3cef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e817c1694fef35ee4836672c0e234d9",
+    "url": "/resume-v1/index.html"
+  },
+  {
+    "revision": "f84a574be0895bb18c0c",
+    "url": "/resume-v1/static/css/main.cac7bbed.chunk.css"
+  },
+  {
+    "revision": "7fe3047280e82ebf243f",
+    "url": "/resume-v1/static/js/2.bb83dafb.chunk.js"
+  },
+  {
+    "revision": "e1cc8c46a72ae73ac18f88913bf71afe",
+    "url": "/resume-v1/static/js/2.bb83dafb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f84a574be0895bb18c0c",
+    "url": "/resume-v1/static/js/main.a0dfc6b2.chunk.js"
+  },
+  {
+    "revision": "158fbf13a0be04acc9cc",
+    "url": "/resume-v1/static/js/runtime-main.c427ae5a.js"
+  },
+  {
+    "revision": "b710a48b6e7af63be95eb7b504a08573",
+    "url": "/resume-v1/static/media/me.b710a48b.jpg"
+  }
+]);
