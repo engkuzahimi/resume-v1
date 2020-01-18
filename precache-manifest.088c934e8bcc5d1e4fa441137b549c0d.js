0793c8f55218afd410ac2259e672cea34b4687c5
Zahimi <engkuzahimi@gmail.com>
2020-01-19 00:29:33 +0800

    Updates

--- precache-manifest.088c934e8bcc5d1e4fa441137b549c0d.js
+++ precache-manifest.088c934e8bcc5d1e4fa441137b549c0d.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e04a03d831cf45248b3279dfacc0b652",
+    "revision": "8ab453c4fd9d2b945ec352290ecc96a9",
     "url": "/resume-v1/index.html"
   },
   {
-    "revision": "4134687bb5a2e49dce6c",
-    "url": "/resume-v1/static/css/main.0fe90444.chunk.css"
+    "revision": "3c3c390d5a397b23a927",
+    "url": "/resume-v1/static/css/main.77fdf64d.chunk.css"
   },
   {
     "revision": "e86c21a244d03c6e3638",
@@ -16,15 +16,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/resume-v1/static/js/2.c15867d4.chunk.js.LICENSE"
   },
   {
-    "revision": "4134687bb5a2e49dce6c",
-    "url": "/resume-v1/static/js/main.00374e4f.chunk.js"
+    "revision": "3c3c390d5a397b23a927",
+    "url": "/resume-v1/static/js/main.c2db89c0.chunk.js"
   },
   {
     "revision": "158fbf13a0be04acc9cc",
     "url": "/resume-v1/static/js/runtime-main.c427ae5a.js"
   },
   {
-    "revision": "b710a48b6e7af63be95eb7b504a08573",
-    "url": "/resume-v1/static/media/me.b710a48b.jpg"
+    "revision": "aa80fda2464fb1230e7ae5ccd7074a43",
+    "url": "/resume-v1/static/media/me.aa80fda2.jpg"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/resume-v1/precache-manifest.511f55aa51273e5acb981bfca9b8515a.js"
+  "/resume-v1/precache-manifest.088c934e8bcc5d1e4fa441137b549c0d.js"
 );
 
 self.addEventListener('message', (event) => {
